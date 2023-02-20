2bdf34a7bef87ad718b9300efe547ffc57aa43b9
Sami <sami-la98@hotmail.fr>
2023-02-20 16:05:12 +0100

    adding component and assets

--- proxxie_app/src/app/app.module.ts
+++ proxxie_app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    SignupComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
